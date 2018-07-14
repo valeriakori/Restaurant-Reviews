dbd43e80753a07cae0d57fd77a8701192e1e1dbc
valeria <a.valeria.koriatchenko@freenet.de>
2018-07-14 14:25:28 +0200

    Initial Commit

---
+++ js/indexController.js
@@ -0,0 +1,11 @@
+// export default function IndexController(container) {
+//   this._registerServiceWorker();
+// }
+
+// IndexController.prototype._registerServiceWorker = function() {
+//     if(!navigator.serviceWorker) return
+
+//     navigator.serviceWorker.register('sw.js')
+//     .then(() => {console.log("Registered")})
+//     .catch((error) => {console.log("Failed: " + error)})
+// };
