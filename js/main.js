e9e118435ac9e23de5654062f460952a7bb1410c
valeria <a.valeria.koriatchenko@freenet.de>
2018-07-14 14:27:18 +0200

    Mobile view

--- js/main.js
+++ js/main.js
@@ -157,6 +157,7 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  */
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
+  const liDiv = document.createElement('div');
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
@@ -165,20 +166,22 @@ createRestaurantHTML = (restaurant) => {
 
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  liDiv.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  liDiv.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  liDiv.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  liDiv.append(more)
+
+  li.append(liDiv);
 
   return li
 }
