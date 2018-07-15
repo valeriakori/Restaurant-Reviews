de1721c7e3376f48e9459642700ab66b7f5b2710
valeria <a.valeria.koriatchenko@freenet.de>
2018-07-15 12:48:39 +0200

    Add MapBox API key

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken:'pk.eyJ1IjoiYW5udmFsa29yIiwiYSI6ImNqamhoNmVwbTVqaGUzcXAxc3FzZHBvYTkifQ.Qn5sYi5Mk34z1K9CKCsHnw',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
