3540dc35c8b7084b5989f25ea98fe47d0c0f151b
valeria <a.valeria.koriatchenko@freenet.de>
2018-07-16 21:09:07 +0200

    open cache

--- sw/sw.js
+++ sw/sw.js
@@ -1,3 +1,17 @@
-self.addEventListener('fetch', function(event) {
-    console.log(event.request);
+self.addEventListener('install', (evt) => {
+    
+    //let cacheName = 'static-cache-v1';
+    let urlsToCache = [
+    '/skeleton',
+    '//normalize-css.googlecode.com/svn/trunk/normalize.css',
+    ''
+    ];
+
+    evt.waitUntil(
+        caches.open('cacheName').then((cache) => {
+            //console.log('cache activated')
+            return cache.addAll(urlsToCache);
+        }).catch((event) => console.log(event))
+    );
 });
+
