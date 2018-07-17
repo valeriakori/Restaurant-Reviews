8e45764618c06cd65cd07ae473658d556d97d9b5
valeria <a.valeria.koriatchenko@freenet.de>
2018-07-17 11:51:29 +0200

    Add request handeling

--- sw/sw.js
+++ sw/sw.js
@@ -8,10 +8,28 @@ self.addEventListener('install', (evt) => {
     ];
 
     evt.waitUntil(
-        caches.open('cacheName').then((cache) => {
+        caches.open('cacheName')
+        .then((cache) => {
             //console.log('cache activated')
             return cache.addAll(urlsToCache);
-        }).catch((event) => console.log(event))
+        })
+        .catch((event) => console.log(event))
     );
 });
 
+self.addEventListener('fetch', (evt) => {
+
+    let requestUrl = new URL(evt.request.url);
+
+    if (requestUrl.origin === location.origin) {
+        if (requestUrl.pathname === '/') {
+            evt.respondWith(caches.match('skeleton'))
+        }
+    }
+
+    evt.respondWith(caches.match(evt.request)
+    .then((response) => {
+        return response || fetch(evt.request);
+    }));
+
+});
