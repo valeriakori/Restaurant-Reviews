fadd8e68d3771364dba8597b7d8d735ff853fba3
valeria <a.valeria.koriatchenko@freenet.de>
2018-07-18 12:46:43 +0200

    Add correct urlsToCache

--- sw.js
+++ sw.js
@@ -1,24 +1,23 @@
-let staticCacheName = 'static-cache-v4';
+let staticCacheName = 'static-cache-v9';
 let urlsToCache = [
     '/',
-    '/skeleton',
-    '//normalize-css.googlecode.com/svn/trunk/normalize.css',
-    'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
-    './css/styles.css',
-    './data/restaurant.json',
-    './js/dbhelper.js',
-    './js/main.js',
-    './js/restaurant_info.js',
-    './img/1.jpg',
-    './img/2.jpg',
-    './img/3.jpg',
-    './img/4.jpg',
-    './img/5.jpg',
-    './img/6.jpg',
-    './img/7.jpg',
-    './img/8.jpg',
-    './img/9.jpg',
-    './img/10.jpg'
+    'index.html',
+    'restaurant.html',
+    'css/styles.css',
+    '/js/main.js',
+    '/js/dbhelper.js',
+    '/js/restaurant_info.js',
+    'data/restaurants.json',
+    'img/1.jpg',
+    'img/2.jpg',
+    'img/3.jpg',
+    'img/4.jpg',
+    'img/5.jpg',
+    'img/6.jpg',
+    'img/7.jpg',
+    'img/8.jpg',
+    'img/9.jpg',
+    'img/10.jpg',
     ];
    
 self.addEventListener('install', (event) => {
