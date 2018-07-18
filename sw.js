6d1a9a3465ae11448c835aa63d6b4a1189182319
valeria <a.valeria.koriatchenko@freenet.de>
2018-07-18 12:28:22 +0200

    Update static cache

--- sw.js
+++ sw.js
@@ -1,23 +1,24 @@
-let staticCacheName = 'static-cache-v1';
+let staticCacheName = 'static-cache-v4';
 let urlsToCache = [
+    '/',
     '/skeleton',
     '//normalize-css.googlecode.com/svn/trunk/normalize.css',
     'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
-    'css/styles.css',
-    'data/restaurant.json',
-    'js/dbhelper.js',
-    'js/main.js',
-    'restaurant_info.js',
-    'img/1.jpg',
-    'img/2.jpg',
-    'img/3.jpg',
-    'img/4.jpg',
-    'img/5.jpg',
-    'img/6.jpg',
-    'img/7.jpg',
-    'img/8.jpg',
-    'img/9.jpg',
-    'img/10.jpg'
+    './css/styles.css',
+    './data/restaurant.json',
+    './js/dbhelper.js',
+    './js/main.js',
+    './js/restaurant_info.js',
+    './img/1.jpg',
+    './img/2.jpg',
+    './img/3.jpg',
+    './img/4.jpg',
+    './img/5.jpg',
+    './img/6.jpg',
+    './img/7.jpg',
+    './img/8.jpg',
+    './img/9.jpg',
+    './img/10.jpg'
     ];
    
 self.addEventListener('install', (event) => {
